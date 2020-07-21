cee70f9800563b4944548511a4efa106058875a0
Sainitin <s.daverpally@gmail.com>
2020-07-20 23:43:28 -0400

    Daily Excercises: Day 2

---
+++ Daily Excercises/day_2.py
@@ -0,0 +1,47 @@
+#1. Using indexing, retrieve the string ‘willow’ from the list and assign that to the variable plant.
+
+data = ['bagel', 'cream cheese', 'breakfast', 'grits', 'eggs', 'bacon', [34, 9, 73, []], [['willow', 'birch', 'elm'], 'apple', 'peach', 'cherry']]
+
+plant = data[7][0][0]
+
+#2. Extract the value associated with the key color and assign it to the variable color. Do not hard code this.
+
+info = {'personal_data':
+         {'name': 'Lauren',
+          'age': 20,
+          'major': 'Information Science',
+          'physical_features':
+             {'color': {'eye': 'blue',
+                        'hair': 'brown'},
+              'height': "5'8"}
+         },
+       'other':
+         {'favorite_colors': ['purple', 'green', 'blue'],
+          'interested_in': ['social media', 'intellectual property', 'copyright', 'music', 'books']
+         }
+      }
+
+color = info['personal_data']['physical_features']['color']
+
+#3. Below, we have provided a list of lists named L. Use nested iteration to save every string containing “b” into a new list named b_strings.
+
+L = [['apples', 'bananas', 'oranges', 'blueberries', 'lemons'], ['carrots', 'peas', 'cucumbers', 'green beans'], ['root beer', 'smoothies', 'cranberry juice']]
+
+b_strings = []
+
+for sub_list in L:
+    for thing in sub_list:
+        if 'b' in thing:
+            b_strings.append(thing)
+
+#4. Write code to assign to the variable map_testing all the elements in lst_check while adding the string “Fruit: ” to the beginning of each element using mapping.
+
+lst_check = ['plums', 'watermelon', 'kiwi', 'strawberries', 'blueberries', 'peaches', 'apples', 'mangos', 'papaya']
+
+map_testing = list(map(lambda x: 'Fruit: ' + x, lst_check))
+
+#5. Below, we have provided a list of strings called countries. Use filter to produce a list called b_countries that only contains the strings from countries that begin with B.
+
+countries = ['Canada', 'Mexico', 'Brazil', 'Chile', 'Denmark', 'Botswana', 'Spain', 'Britain', 'Portugal', 'Russia', 'Thailand', 'Bangladesh', 'Nigeria', 'Argentina', 'Belarus', 'Laos', 'Australia', 'Panama', 'Egypt', 'Morocco', 'Switzerland', 'Belgium']
+
+b_countries = list(filter(lambda country: country[0] == 'B', countries))
